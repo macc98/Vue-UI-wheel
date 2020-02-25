78084efad3f908660a4be39d610e183fe46b30b5
macc98 <318659989@qq.com>
2020-02-25 23:01:16 +0800

    添加buttonGroup组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './buttonGroup'
 
 Vue.component('m-button', Button)
 Vue.component('m-icon',Icon)
+Vue.component('m-button-group',ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/buttonGroup.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="m-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="scss">
+    .m-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        border-radius: var(--border-radius);
+        & .m-button{
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child{
+                border-radius:var(--border-radius) 0 0 var(--border-radius) ;
+            }
+            &:last-child{
+                border-radius: 0 var(--border-radius) var(--border-radius) 0;
+            }
+            &:hover{
+                z-index: 1;
+            }
+        }
+    }
+</style>
