e0dba817e5fcffcc5ff786d268f2db2bde8cb032
macc98 <318659989@qq.com>
2020-02-29 22:14:38 +0800

    添加col、row组件，简单实现栅格布局

---
+++ src/col.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col" :class="[span && `col-${span}` , offset && `offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border: 1px solid red;height: 50px;">
+            <slot></slot>
+        </div>
+
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return{
+                gutter:0
+            }
+        },
+        props:{
+            span:{
+                type:[String,Number],
+            },
+            offset:{
+                type:[String,Number]
+            },
+
+        }
+    }
+</script>
+<style lang="scss" scoped>
+
+    .col{
+        width:50%;
+
+        $class: offset-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                margin-left: ($n/24) * 100%;
+            }
+        }
+
+        $class: col-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                width: ($n/24) * 100%;
+            }
+        }
+    }
+</style>
